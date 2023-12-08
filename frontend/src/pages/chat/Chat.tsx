c2272f5623a13946d0872441d1164a917f23c639
dominikbrand <63984069+dominikbrand@users.noreply.github.com>
2023-12-08 02:00:18 +0100

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -585,8 +585,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Unterhaltung starten</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Gerne versuche ich deine Frage zu beantworten</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
@@ -620,7 +620,7 @@ const Chat = () => {
                                         <div className={styles.chatMessageGpt}>
                                             <Answer
                                                 answer={{
-                                                    answer: "Generating answer...",
+                                                    answer: "Generiere Antwort...",
                                                     citations: []
                                                 }}
                                                 onCitationClicked={() => null}
